5972a0d119623346bdbf9c63379a4ac6eb63bce9
Byung Moon Kim <kimbm2014@gmail.com>
2021-01-26 23:31:24 -0800

    add IBOutlets in TipCalculatorViewController class

--- MyTipCalculator/TipCalculatorViewController.swift
+++ MyTipCalculator/TipCalculatorViewController.swift
@@ -1,1 +1,30 @@
+//
+//  TipCalculatorViewController.swift
+//  MyTipCalculator
+//
+//  Created by Byung Moon Kim on 1/26/21.
+//
 
+import UIKit
+
+class TipCalculatorViewController:
+    UIViewController{
+    
+   
+    @IBOutlet weak var amountBeforeTaxTextField:
+        UITextField!
+    @IBOutlet weak var tipPercentageLabel:
+        UILabel!
+    @IBOutlet weak var tipPercentageSlider:
+        UISlider!
+    @IBOutlet weak var numOfPeopleSlider:
+        UISlider!
+    @IBOutlet weak var numOfPeopleLabel:
+        UILabel!
+    
+    @IBOutlet weak var eachPersonAmount:
+        UILabel!
+    @IBOutlet weak var totalResultLabel:
+        UILabel!
+    
+}
